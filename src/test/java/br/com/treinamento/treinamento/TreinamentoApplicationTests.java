90a2fd7dfa792d2013f458446da12f7d40474bab
Thiago Silva <thiago-silva@ultracar.org>
2019-12-06 16:39:58 -0300

    TESTE

--- src/test/java/br/com/treinamento/treinamento/TreinamentoApplicationTests.java
+++ src/test/java/br/com/treinamento/treinamento/TreinamentoApplicationTests.java
@@ -1,6 +1,6 @@
 package br.com.treinamento.treinamento;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.core.MediaType;
 
@@ -13,12 +13,9 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.ultracar.treinamento.TreinamentoApplication;
-import br.com.ultracar.treinamento.entidades.Estado;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import junit.framework.Assert;
 import net.minidev.json.JSONObject;
 
 @RunWith(SpringRunner.class)
@@ -30,19 +27,27 @@ public class TreinamentoApplicationTests {
 	
 	@Before
 	public void setUp() {
-		RestAssured.baseURI = "http://localhost:8082/api";
+		RestAssured.baseURI = "http://localhost:"+port+"/api";
 	}
 	
 	@Test
 	public void contextPostEstado() throws JSONException {
 		RequestSpecification request = RestAssured.given().contentType(MediaType.APPLICATION_JSON);
-		JSONObject requestParam = new JSONObject();
-		
+		JSONObject requestParam = new JSONObject();		
 		requestParam.put("sigla", "MG");
 		request.body(requestParam.toJSONString());
-		Response response = request.post("/estados");
-		
+		Response response = request.post("/estados");	
 		Integer statusCode = response.getStatusCode();
-		assertEquals(statusCode, 201);
+		assertEquals(Long.parseLong(statusCode.toString()), 201);
 	}
+	
+	@Test
+	public void ContextGetEstado() {
+		RequestSpecification request = RestAssured.given().contentType(MediaType.APPLICATION_JSON);
+		Response response = request.get("/estados");
+		Integer statusCode = response.getStatusCode();
+		assertEquals(Long.parseLong(statusCode.toString()), 200);
+	}
+	
+	
 }
